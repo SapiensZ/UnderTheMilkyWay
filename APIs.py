b501e91aafa50e33caa348de688164324893e126
Ruixu Chen <moneyes1997@gmail.com>
2019-11-12 22:34:00 +0100

    IMDB API

---
+++ APIs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 16:13:29 2019
+
+@author: Money
+"""
+
+import requests
+import json
+    
+def search_by_title(title):
+    #The APi is provided by www.omdbapi.com
+    
+    url = "http://www.omdbapi.com/?t={0}&apikey=9adcf453".format(title)
+    
+    response = requests.get(url)
+
+    res = json.loads(response.text)
+    return res
+
+
+search_by_title('Les trois mousquetaires')
